15fc8307bed77db48f5ed67c28237ebc5d085dbb
DonutDoggoBoiz <95366166+DonutDoggoBoiz@users.noreply.github.com>
2022-09-24 18:34:35 +0700

    Update 03_create_model.py

--- pages/03_create_model.py
+++ pages/03_create_model.py
@@ -24,7 +24,7 @@ if st.button('Observe'):
     df_price.drop(columns=['Adj Close','Volume'] , inplace=True)
     # st.line_chart(df_price['Close'])
     # c = alt.Chart(df_price['Close']).mark_line()  #.encode(x='Date',y='Price')
-    st.write(df_price['Close'])
+    st.pyplot(df_price['Close'].plot(kind='line', color="tab:orange"))
     
 else:
     st.write('Click "Observe" button to observe historical price chart')
