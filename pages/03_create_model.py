5596bd3aa104ab32d4f9dbd3ae71050f5846f20b
DonutDoggoBoiz <95366166+DonutDoggoBoiz@users.noreply.github.com>
2022-09-24 17:17:15 +0700

    Update 03_create_model.py

--- pages/03_create_model.py
+++ pages/03_create_model.py
@@ -5,3 +5,8 @@ import streamlit as st
 
 st.title('Create DQN Trading Model 🚀')
 st.sidebar.markdown('## Create Model 🚀')
+
+stock_name = st.selectbox('Select your Stock', ('BBL', 'PTT', 'ADVANC','KBANK') )
+
+x = [1,2,3,4,5,6,7,8,9,10]
+st.write(x)
