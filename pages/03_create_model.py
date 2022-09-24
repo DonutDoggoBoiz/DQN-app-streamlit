b01973402221c670fed57872a072140855096423
DonutDoggoBoiz <95366166+DonutDoggoBoiz@users.noreply.github.com>
2022-09-24 17:08:50 +0700

    Update 03_create_model.py

--- pages/03_create_model.py
+++ pages/03_create_model.py
@@ -1,4 +1,7 @@
 import streamlit as st
 
-st.markdown("# Create MODEL 🚀")
-st.sidebar.markdown("# Create Model 🚀")
+# st.markdown("# Create MODEL 🚀")
+# st.sidebar.markdown("# Create Model 🚀")
+
+st.title('Create DQN Trading Model 🚀')
+st.sidebar.markdown('## Create Model 🚀')
