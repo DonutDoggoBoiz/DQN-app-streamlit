757f445e64876126b43fea2960e29fd521021be9
DonutDoggoBoiz <95366166+DonutDoggoBoiz@users.noreply.github.com>
2022-09-25 18:44:57 +0700

    Update 01_login.py

--- pages/01_login.py
+++ pages/01_login.py
@@ -3,28 +3,5 @@ import streamlit as st
 st.markdown("# This is a Login Page 🔑")
 st.sidebar.markdown("# Login 🔑")
 
-"""
-names = ["Peter Parker", "Rebecca Miller"]
-usernames = ["pparker", "rmiller"]
-passwords = ["abc123", "def456"]
-
-authenticator = stauth.Authenticate(names, usernames, passwords,
-                                    "sales_dashboard", "abcdef", cookie_expiry_days=0)
-name, authentication_status, username = authenticator.login("Login", "main")
-
-if authentication_status == False:
-  st.error("Username/password is incorrect")
-
-if authentication_status == None:
-  st.warning("Please enter your username and password")
-
-if authentication_status:
-  st.write("### Login Success")
-  st.write("### Welcome to our app!")
-  
-  # --- SIDEBAR ---
-  authenticator.logout("Logout", "sidebar")
-  st.sidebar.title(f"Welcome {name}")
-"""
 
 st.write('----None----')
