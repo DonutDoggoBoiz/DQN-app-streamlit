67adaf235338b09bdf33c345cc41674d578b4fdf
DonutDoggoBoiz <95366166+DonutDoggoBoiz@users.noreply.github.com>
2022-09-25 19:24:14 +0700

    Update 01_login.py

--- pages/01_login.py
+++ pages/01_login.py
@@ -1,7 +1,52 @@
 import streamlit as st
 
-st.markdown("# This is a Login Page 🔑")
-st.sidebar.markdown("# Login 🔑")
+#st.markdown("### This is a Login Page 🔑")
+#st.sidebar.markdown("### Login 🔑")
+#st.write('--------')
 
+# --- mocked up User Database ---
+if st.sidebar.checkbox('database'):
+  users = ['admin']
+  passwords = ['admin']
 
-st.write('----None----')
+st.title("Simple Login App")
+
+menu = ["Home", "Login", "Sign Up"]
+choice = st.sidebar.selectbox("Menu", menu)
+
+if choice == "Home":
+  st.subheader("Home")
+  
+elif choice == "Login":
+  st.subheader("Login Section")
+  username = st.sidebar.test_input("Username")
+  password = st.sidebar.text_input("Password", type='password')
+  if st.sidebar.checkbox("Login"):
+    #if password == '12345':
+    if passwords.index(users) == password:
+      st.success("Logged In as {}".format(username))
+
+      task = st.selectbox("Task", ["Add Post", "Analytics", "Profiles"])
+      if task == "Add Post":
+        st.subheader("Add Your Post")
+      elif task == "Analytics":
+        st.subheader("Analytics")
+      elif task == "Profiles":
+        st.subheader("Profiles")
+        users_df = pd.DataFrame({'users':users,
+                                 'passwords':passwords}
+                               )
+        st.dataframe(users_df)
+    else:
+      st.warning("Incorrect Username/Password")
+  
+elif choice == "Sign Up":
+  st.subheader("Create New Account")
+  new_user = st.text_input("Username")
+  new_password = st.text_input("Password", type='password')
+  
+  if st.button("Signup"):
+    users.append(new_user)
+    passwords.append(new_password)
+    st.success("You have successfully created a valid account")
+    st.info("Go to Login Menu to login")
