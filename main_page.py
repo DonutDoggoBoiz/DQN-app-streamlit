81cbefc7c562723cb9662d13677f086d81be01c9
DonutDoggoBoiz <95366166+DonutDoggoBoiz@users.noreply.github.com>
2022-09-25 17:38:54 +0700

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -1,8 +1,7 @@
 # Contents of ~/my_app/main_page.py
 import streamlit as st
+import streamlit_authenticator as stauth
 
 st.markdown("# Welcome to our Home page 🎉")
 st.markdown("💸💸💸")
 st.sidebar.markdown("# Home page 🎉")
-
-print("hellop PRINT")
