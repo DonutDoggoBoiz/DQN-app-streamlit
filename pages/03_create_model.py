bdc18abc674c2cd1430088edbfb30ae87abdab3e
DonutDoggoBoiz <95366166+DonutDoggoBoiz@users.noreply.github.com>
2022-09-26 20:45:44 +0700

    Update 03_create_model.py

--- pages/03_create_model.py
+++ pages/03_create_model.py
@@ -14,6 +14,7 @@ st.sidebar.markdown('## Create Model 💡')
 
 set_param_button = st.button("Set Parameters")
 if set_param_button:
+        st.write"#### Create a form of widgets to set parameters")
         st.write("Setting parameters .....")
         #set_parameters()
         #st.write("action_space: {}".format(action_space) ) 
@@ -23,31 +24,31 @@ if set_param_button:
         st.write("Setting parameters B")
         st.write("Setting parameters C")
         '''
-        ### --- environment parameters
+        --- environment parameters
         action_space = 2
         window_size = 5
         n_episodes = 2
 
-        ### --- agent parameters
+        --- agent parameters
         agent_gamma = 0.99 
         agent_epsilon = 1.0
         agent_epsilon_dec = 0
         agent_epsilon_end = 0.01
         agent_lr = 0.001
 
-        ### --- trading parameters
+        --- trading parameters
         initial_balance = 1000000
         trading_size_pct = 10
         commission_fee_pct = 0.157
         trade_size = (trading_size_pct/100) * initial_balance
         commission_fee = (commission_fee_pct/100) * 1.07
 
-        ### --- episodic History
+        --- episodic History
         total_acc_reward_history = []
         end_balance_history = []
         eps_history = []
 
-        ### --- trading History
+        --- trading History
         acc_reward_history = []
         action_history = []
         account_balance_history = []
