23d14eb0278a8ac5ee14e471ed32e6fbe1592629
DonutDoggoBoiz <95366166+DonutDoggoBoiz@users.noreply.github.com>
2022-09-26 18:20:24 +0700

    Update 00_workbench.py

--- pages/00_workbench.py
+++ pages/00_workbench.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
+def set_sess(value):
+  global st.session_state['sess_momo']
+  st.session_state['sess_momo'] = value
+
 if st.checkbox("Test sess_momo"):
   st.session_state['sess_momo'] = 9999
   sess_momo = st.session_state['sess_momo']
@@ -9,7 +13,8 @@ if st.checkbox("Test sess_momo"):
 if st.checkbox("Test normal momo"):
   momo_paradise = 11
   st.write("start momo = {} ".format(momo_paradise) )
-  st.session_state['sess_momo'] = 1111
+  if st.checkbox("set sess"):
+    set_sess(1111)
 
 if st.checkbox("End momo"):
   st.write('end momo = {}'.format(momo_paradise) )
