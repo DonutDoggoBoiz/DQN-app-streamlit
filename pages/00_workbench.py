9dfec5a323e60f5ee46debd2821265bb5fc6cbde
DonutDoggoBoiz <95366166+DonutDoggoBoiz@users.noreply.github.com>
2022-09-26 18:13:22 +0700

    Update 00_workbench.py

--- pages/00_workbench.py
+++ pages/00_workbench.py
@@ -1,9 +1,12 @@
-st.session_state['sess_momo'] = 9999
-sess_momo = st.session_state['sess_momo']
-st.write("start sess_momo = {} ".format(st.session_state['sess_momo']))
+if st.checkbox("Test sess_momo"):
+  st.session_state['sess_momo'] = 9999
+  sess_momo = st.session_state['sess_momo']
+  st.write("start sess_momo = {} ".format(st.session_state['sess_momo']))
 
-momo_paradise = 99
-st.write("start momo = {} ".format(momo_paradise) )
+if st.checkbox("Test normal momo"):
+  momo_paradise = 99
+  st.write("start momo = {} ".format(momo_paradise) )
 
-st.write('end momo = {}'.format(momo_paradise) )
-st.write('end sess_momo = {}'.format(sess_momo) )
+if st.checkbox("End momo"):
+  st.write('end momo = {}'.format(momo_paradise) )
+  st.write('end sess_momo = {}'.format(sess_momo) )
