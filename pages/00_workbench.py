cdce659e862523d18fd4e6538acba6b714b622a6
DonutDoggoBoiz <95366166+DonutDoggoBoiz@users.noreply.github.com>
2022-09-26 18:17:12 +0700

    Update 00_workbench.py

--- pages/00_workbench.py
+++ pages/00_workbench.py
@@ -4,9 +4,10 @@ if st.checkbox("Test sess_momo"):
   st.session_state['sess_momo'] = 9999
   sess_momo = st.session_state['sess_momo']
   st.write("start sess_momo = {} ".format(st.session_state['sess_momo']))
+  momo_paradise = 99
 
 if st.checkbox("Test normal momo"):
-  momo_paradise = 99
+  momo_paradise = 11
   st.write("start momo = {} ".format(momo_paradise) )
   st.session_state['sess_momo'] = 1111
 
