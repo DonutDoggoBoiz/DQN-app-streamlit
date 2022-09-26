4b6fb026915948fcab1b718420e8ef063ccb783e
DonutDoggoBoiz <95366166+DonutDoggoBoiz@users.noreply.github.com>
2022-09-26 20:05:43 +0700

    Update 02_register.py

--- pages/02_register.py
+++ pages/02_register.py
@@ -7,7 +7,7 @@ register_user_form = st.form('Register user')
 register_user_form.subheader('Registration Form')
 new_username = register_user_form.text_input('Username').lower()
 new_password = register_user_form.text_input('Password', type='password')
-new_password_repeat = register_user_form.text_input('Repeat password', type='password')
+# new_password_repeat = register_user_form.text_input('Repeat password', type='password')
 
 if register_user_form.form_submit_button('Register'):
   if len(new_username) <= 0:
