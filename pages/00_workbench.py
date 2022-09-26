08a658e652dced865b909794e18f0213953dada2
DonutDoggoBoiz <95366166+DonutDoggoBoiz@users.noreply.github.com>
2022-09-26 18:11:51 +0700

    Update 00_workbench.py

--- pages/00_workbench.py
+++ pages/00_workbench.py
@@ -1,2 +1,9 @@
-if st.button('Check "session_state"'):
-  st.session_state
+st.session_state['sess_momo'] = 9999
+sess_momo = st.session_state['sess_momo']
+st.write("start sess_momo = {} ".format(st.session_state['sess_momo']))
+
+momo_paradise = 99
+st.write("start momo = {} ".format(momo_paradise) )
+
+st.write('end momo = {}'.format(momo_paradise) )
+st.write('end sess_momo = {}'.format(sess_momo) )
