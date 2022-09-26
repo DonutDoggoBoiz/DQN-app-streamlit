fc3cf5f2a732f1c91f090d95ef7ffcc613d02c6b
DonutDoggoBoiz <95366166+DonutDoggoBoiz@users.noreply.github.com>
2022-09-26 19:35:45 +0700

    Update 04_model_management.py

--- pages/04_model_management.py
+++ pages/04_model_management.py
@@ -1,16 +1,17 @@
 import streamlit as st
 
-# st.markdown("# View, Edit, Delete Model 🛠️")
-# st.sidebar.markdown("# CRUD model 🛠️")
-
 st.set_page_config(
     page_title="Ex-stream-ly Cool App",
     page_icon="🧊",
     layout="wide",
     initial_sidebar_state="expanded",
     menu_items={
-        'Get Help': 'https://www.extremelycoolapp.com/help',
-        'Report a bug': "https://www.extremelycoolapp.com/bug",
-        'About': "# This is a header. This is an *extremely* cool app!"
-    }
-)
+                'Get Help': 'https://www.extremelycoolapp.com/help',
+                'Report a bug': "https://www.extremelycoolapp.com/bug",
+                'About': "# This is a header. This is an *extremely* cool app!"
+                }
+                    )
+
+st.markdown("## View, Edit, Delete Model 🛠️")
+st.sidebar.markdown("### CRUD model 🛠️")
+st.write("test st.set_page_config")
