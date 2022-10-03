c36515360c8b68bede4adcfeb4aa837923d0fa19
DonutDoggoBoiz <95366166+DonutDoggoBoiz@users.noreply.github.com>
2022-10-03 21:33:51 +0700

    Update 03_create_model2.py

--- pages/03_create_model2.py
+++ pages/03_create_model2.py
@@ -1,25 +1,37 @@
 import streamlit as st
+from functions import fetch_price_data, observe_price, split_dataset, set_parameters, train_model
   
 ### ------------ INTERFACE ------------ ###
-tab1, tab2, tab3, tab4 = st.tabs(["Select Data", "Parameters", "Train Model", "Test Model"])
+tab1, tab2, tab3, tab4 = st.tabs(["Select Data 📈", "Parameters 💡", "Train Model 🚀", "Test Model 🧪"])
 
 with tab1:
-    st.header("Select stock and price range")
-    stock_name = st.selectbox('Select your Stock', ('BBL', 'PTT', 'ADVANC','KBANK') )
-    start_date = st.date_input("Select start date: ", datetime.date(2021, 9, 20))
-    end_date = st.date_input("Select end date: ", datetime.date(2022, 9, 20))
-    stock_code = stock_name + '.BK'
-    df_price = yf.download(stock_code,
-                            start=start_date,
-                            end=end_date,
-                            progress=True)
-    df_price.drop(columns=['Adj Close','Volume'] , inplace=True)
-    df_length = df_price['Close'].count()
+    st.header("Select stock and price range 📈")
+    fetch_price_data()
+    observe_button = st.checkbox('Observe Dataset🔍')
+    if observe_button:
+      observe_price()
+      split_button = st.checkbox("Split dataset ✂️")
+      if split_button:
+        st.write("Spliting.........")
+        split_dataset()
+        st.write("Train dataset")
+        st.line_chart(df_price_train)
+        st.write("Test dataset")
+        st.line_chart(df_price_test)
+        st.write("Spliting......... DONE!")
+        set_param_button = st.checkbox("Set Parameters")
 
 with tab2:
-   st.header("Set parameters for your trading model")
-   
+    st.header("Set parameters for your trading model 💡")
+    set_param_button = st.checkbox("Set Parameters")
+    if set_param_button:
+      set_parameters()
 
 with tab3:
-   st.header("Train your model")
-   
+    st.header("Train your model 🚀")
+    train_button = st.button("Start Training 🏃")
+    if train_button:
+      train_model()
+
+with tab4:
+    st.header("Test Model on test set")
