06bd7c038fb9d5c9c9cc3167d3558f2ead12b13c
DonutDoggoBoiz <95366166+DonutDoggoBoiz@users.noreply.github.com>
2022-10-04 16:33:50 +0700

    Update 03_create_model2.py

--- pages/03_create_model2.py
+++ pages/03_create_model2.py
@@ -3,12 +3,12 @@ import datetime
 from functions import fetch_price_data, observe_price, split_dataset, set_parameters, train_model, split_dataset2, test_model, save_model
 
 ### ------------ INTERFACE ------------ ###
-tab1, tab2, tab3, tab4, tab5 = st.tabs(["Select Data 📈", "Parameters 💡", "Train Model 🚀", "Test Model 🧪", "Save Model 💾"])
+tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["###Select Data 📈", "Parameters 💡", "Train Model 🚀", "Test Model 🧪", "Save Model 💾", "PENDING"])
 
 with tab1:
     st.header("Select stock and price range 📈")
     fetch_price_data()
-    observe_button = st.checkbox('Observe Dataset 🔍')
+    observe_button = st.checkbox('View Dataset 🔍')
     if observe_button:
       observe_price()
       split_button = st.checkbox("Split dataset ✂️")
@@ -18,9 +18,10 @@ with tab1:
 
 with tab2:
     st.header("Set parameters for your trading model 💡")
-    set_param_button = st.checkbox("Set Parameters")
-    if set_param_button:
-      set_parameters()
+    #set_param_button = st.checkbox("Set Parameters")
+    #if set_param_button:
+      #set_parameters()
+    set_parameters()
 
 with tab3:
     st.header("Train your model with train set 🚀")
@@ -41,3 +42,20 @@ with tab5:
     if save_button:
         #save_model()
         st.success("Your model is saved successfully")
+        
+with tab6:
+    st.header("PENDING adjustment...")
+    st.success("select data = DONE")
+    st.warning("parameter -- adjust interface and input choice")
+    st.warning("parameter -- add info to each input")
+    st.warning("train model -- add input field for n_episodes"
+    st.warning("test model -- better result visualization"
+    st.warning("save model -- integrate to cloud infrastructure"
+    st.warning("generate advice -- add load_model function")
+    st.warning("generate advice -- compulsory stock quote")
+    st.warning("generate advice -- formally written buy/sell advice")
+    st.error("overall -- user database and management system")
+    st.error("overall -- stock quote database")
+     
+             
+             
