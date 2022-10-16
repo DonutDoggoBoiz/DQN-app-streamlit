b634a54d89d9489cfcd335af3cac154564cf630e
DonutDoggoBoiz <95366166+DonutDoggoBoiz@users.noreply.github.com>
2022-10-16 22:37:36 +0700

    Update 99_workbench.py

--- pages/99_workbench.py
+++ pages/99_workbench.py
@@ -7,28 +7,6 @@ from deta import Deta
 deta = Deta(st.secrets["deta_key"])
 user_db = deta.Base("user_db")
 
-### --- DEFINE --- ###
-def login_form_gen():
-  login_form = st.form('Login')
-  login_form.subheader('Login Form 📝')
-  username = login_form.text_input('Username', placeholder='your username')
-  password = login_form.text_input('Password', type='password', placeholder='your password')
-  if login_form.form_submit_button('Login'):
-    if len(username) <= 0:
-      st.warning("Please enter a username")
-    elif len(password) <= 0:
-      st.warning("Please enter your password")
-    elif len(username) > 0 and len(password) > 0:
-      if username not in user_list:
-        st.warning('User not found. Please check your username')
-      else: 
-        if user_frame.loc[user_frame['username'] == username,'password'].values != password:
-          st.error("Password incorrect. Please try again")
-        else:
-          st.success("Login Successful!")
-          st.write('Welcome, {}'.format(username))
-
-
 ### --- INITIALIZE DF AND LIST --- ###
 user_frame = pd.DataFrame(user_db.fetch().items)
 user_list = user_frame['username'].values.tolist()
@@ -61,7 +39,3 @@ if st.button('show form') or st.session_state['show_form_state']:
         else:
           st.success("Login Successful!")
           st.write('Welcome, {}'.format(username))
-
-if st.button('show form 2') or st.session_state['show_form_state']:
-  st.session_state['show_form_state'] = True
-  login_form_gen()
