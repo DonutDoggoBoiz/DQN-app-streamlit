857861e70200d39cde5fd756d72ef97e6a2f45e0
DonutDoggoBoiz <95366166+DonutDoggoBoiz@users.noreply.github.com>
2022-10-17 18:10:38 +0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -13,7 +13,7 @@ deta = Deta(st.secrets["deta_key"])
 stock_db = deta.Base("stock_db")
 
 stock_df = pd.DataFrame(stock_db.fetch().items)
-stock_list = stock_df['symbol'].values.to_list().sort()
+stock_list = stock_df['symbol'].values
 #stock_list_sorted = stock_list.sort()
 
 # ---------------------------- #
