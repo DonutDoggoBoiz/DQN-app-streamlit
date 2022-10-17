14f94dc075159a24549151d87085d01cf472ed27
DonutDoggoBoiz <95366166+DonutDoggoBoiz@users.noreply.github.com>
2022-10-17 19:49:21 +0700

    Update 99_workbench2.py

--- pages/99_workbench2.py
+++ pages/99_workbench2.py
@@ -11,24 +11,32 @@ if st.button('test st.empty()'):
 
 
 ### --- TEST placeholder = st.empty --- ###
-#placeholder = st.empty()
+# BUTTONS
+test_b =st.button('text'):
+line_chart_b = st.button('line chart'):
+container_b = st.button('container'):       
+clear_b = st.button('clear'):
 
+# SINGLE ELEMENT CONTAINER
+placeholder = st.empty()
+
+# BUTTON CONDITIONS
 # Replace the placeholder with some text:
-if st.button('text'):
+if test_b:
     placeholder.text("Hello")
 
-if st.button('line chart'):
+if line_chart_b:
     # Replace the text with a chart:
     placeholder.line_chart({"data": [1, 5, 2, 6]})
 
-if st.button('container'):
+if container_b:
     # Replace the chart with several elements:
     with placeholder.container():
         st.write("This is one element")
         st.write("This is another")
         
-if st.button('clear'):
+if clear_b:
     # Clear all those elements:
     placeholder.empty()
     
-placeholder = st.empty()
+
