e608e08221de7986de0eb0ec8ba61944adb04f17
DonutDoggoBoiz <95366166+DonutDoggoBoiz@users.noreply.github.com>
2022-10-31 17:32:41 +0700

    Update 99_workbench3.py

--- pages/99_workbench3.py
+++ pages/99_workbench3.py
@@ -15,3 +15,20 @@ data_dict = {'model_name': ['bbl_01','bbl_02','ppt_05','scg_111','mint_01'],
             }
 data_df = pd.DataFrame(data_dict)
 st.dataframe(data_df)
+
+#-------
+gb = GridOptionsBuilder.from_dataframe(data_df)
+#gb.configure_pagination(paginationAutoPageSize=True) #Add pagination
+#gb.configure_side_bar() #Add a sidebar
+gb.configure_selection('single', use_checkbox=True)
+gridoptions = gb.build()
+
+grid_response = AgGrid(data_df,
+                       gridOptions=gridoptions)
+
+data = grid_response['data']
+selected_data = grid_response['selected_rows'] 
+#df = pd.DataFrame(selected) #Pass the selected rows to a new dataframe df
+st.write(selected_data)
+with st.expander('data'):
+  st.write(data)
