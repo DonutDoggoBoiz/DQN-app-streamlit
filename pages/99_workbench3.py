7e998a9f5877d605582af154722581a359fe9f83
DonutDoggoBoiz <95366166+DonutDoggoBoiz@users.noreply.github.com>
2022-10-31 17:09:46 +0700

    Update 99_workbench3.py

--- pages/99_workbench3.py
+++ pages/99_workbench3.py
@@ -3,9 +3,15 @@ import streamlit as st
 import pandas as pd
 from deta import Deta
 import time
-from functions import login_form
+#streamlit-aggrid
+from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
+### ---------------------- ###
 
-login_form()
-
-if st.sidebar.button('check sess'):
-  st.write('check session state, {}'.format(st.session_state['username']))
+data_dict = {'model_name': ['bbl_01','bbl_02','ppt_05','scg_111','mint_01'],
+             'gamma': ['A','B','C','D','E'],
+             'learning_rate': [0.001,0.002,0.005,0.04,0.099]
+             'initial_balance': [1000000,1200000,1980000,2550000,3390000],
+             'trading_size': [0.10,0.25,0.15,0.30,0.50]
+            }
+data_df = pd.DataFrame(data_dict)
+st.dataframe(data_df)
