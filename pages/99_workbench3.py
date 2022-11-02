af0f3dc9196710ffe070e5911f59a181c0dc807b
DonutDoggoBoiz <95366166+DonutDoggoBoiz@users.noreply.github.com>
2022-11-02 21:58:06 +0700

    Update 99_workbench3.py

--- pages/99_workbench3.py
+++ pages/99_workbench3.py
@@ -13,28 +13,28 @@ if 'del_mod_button_status' not in st.session_state:
 ### ---------------------- ###
 
 
-data_dict = {'model_name': ['bbl_01','bbl_02','ppt_05','scg_111','mint_01'],
+datamodel_dict = {'model_name': ['bbl_01','bbl_02','ppt_05','scg_111','mint_01'],
              'gamma': [0.90,0.80,0.85,0.75,0.95],
              'learning_rate': [0.001,0.002,0.005,0.04,0.099],
              'initial_balance': [1000000,1200000,1980000,2550000,3390000],
              'trading_size': [0.10,0.25,0.15,0.30,0.50]
             }
-data_df = pd.DataFrame(data_dict)
-st.dataframe(data_df)
+datamodel_df = pd.DataFrame(datamodel_dict)
+st.dataframe(datamodel_df)
 
 #-------
-gb = GridOptionsBuilder.from_dataframe(data_df)
+gb = GridOptionsBuilder.from_dataframe(datamodel_df)
 #gb.configure_pagination(paginationAutoPageSize=True) #Add pagination
 #gb.configure_side_bar() #Add a sidebar
 gb.configure_selection('single', use_checkbox=True, pre_selected_rows=[0])
 gridoptions = gb.build()
 
-grid_response = AgGrid(data_df,
+grid_response = AgGrid(datamodel_df,
                        fit_columns_on_grid_load=True,
                        gridOptions=gridoptions)
 
-data = grid_response['data']
-selected_data = grid_response['selected_rows'] 
+grp_data = grid_response['data']
+selected_row = grid_response['selected_rows'] 
 
 placeholder = st.empty()
 placeholder2 = st.empty()
@@ -58,7 +58,7 @@ if del_mod_button or st.session_state['del_mod_button_status']:
       if confirm_button:
         if make_sure_radio == 'Yes':
           st.session_state['del_mod_button_status'] = False
-          st.error('Model {} has been successfully deleted'.format(selected_data[0]['model_name']))
+          st.error('Model {} has been successfully deleted'.format(selected_row[0]['model_name']))
           time.sleep(3)
           st.experimental_rerun()
         elif make_sure_radio == 'No':
@@ -72,11 +72,11 @@ try:
   placeholder.empty()
   with placeholder.container():
     with st.expander('More model information:'):
-        st.write('Name : {}'.format(selected_data[0]['model_name']))
-        st.write('Gamma : {:.2f}'.format(selected_data[0]['gamma']))
-        st.write('Learning Rate : {:.3f}'.format(selected_data[0]['learning_rate']))
-        st.write('Initial Balance : {:,} THB'.format(selected_data[0]['initial_balance']))
-        st.write('Trading Size : {:.2f}%'.format(selected_data[0]['trading_size']*100))
+        st.write('Name : {}'.format(selected_row[0]['model_name']))
+        st.write('Gamma : {:.2f}'.format(selected_row[0]['gamma']))
+        st.write('Learning Rate : {:.3f}'.format(selected_row[0]['learning_rate']))
+        st.write('Initial Balance : {:,} THB'.format(selected_row[0]['initial_balance']))
+        st.write('Trading Size : {:.2f}%'.format(selected_row[0]['trading_size']*100))
 except:
   with placeholder.container():
     st.success('Loading...')
