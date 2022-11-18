e760abea42c5d398aec1eefbffff230b3e783b13
DonutDoggoBoiz <95366166+DonutDoggoBoiz@users.noreply.github.com>
2022-11-18 15:56:52 +0700

    Update 99_workbench.py

--- pages/99_workbench.py
+++ pages/99_workbench.py
@@ -33,12 +33,19 @@ if gcs_switch:
   local_path2 = 'models/gcs_mnist_test2.csv'
   content = bucket.blob(file_path).download_to_filename(local_path)
   content2 = bucket.blob(file_path).download_as_string()
-  show_gcs_file = st.button('Show GCS file')
-  show_local_file = st.button('Show local file')
-  if show_gcs_file:
+  
+show_gcs_file = st.button('Show GCS file')
+show_local_file = st.button('Show local file')
+if show_gcs_file:
+  try:
     #gcs_df = pd.read_csv(content2)
     #st.dataframe(gcs_df)
     st.write(str(content2))
-  if show_local_file:
+  except:
+    st.error('ERROR GCS')
+if show_local_file:
+  try:
     local_df = pd.read_csv(local_path)
     st.dataframe(local_df)
+  except:
+    st.error('ERROR LOCAL')
