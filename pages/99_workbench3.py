06f39f57751658d39801d92689c7419b305b3e37
DonutDoggoBoiz <95366166+DonutDoggoBoiz@users.noreply.github.com>
2022-11-19 22:54:15 +0700

    Update 99_workbench3.py

--- pages/99_workbench3.py
+++ pages/99_workbench3.py
@@ -102,11 +102,13 @@ if show_model_frame:
 if show_model_frame2:
   st.dataframe(model_frame2)
 if write_selected_row:
-  detour_key = model_frame2.loc[model_frame2['model_name']==selected_row[0]['model_name'],'key'].values
+  detour_key_pd = model_frame2.loc[model_frame2['model_name']==selected_row[0]['model_name'],'key']
+  detour_key_list = detour_key_pd.to_list()
+  detour_key_object = detour_key_list[0]
   st.write('selected_row model_name: {}'.format(selected_row[0]['model_name']))
-  st.write('selected_row detour key: {}'.format(detour_key))
-  st.write('detour key type: {}'.format(type(detour_key)))
-  st.write('str detour key: {}'.format(str(detour_key)))
+  st.write('selected_row detour key panda: {}, type: {}'.format(detour_key_pd, type(detour_key_pd)))
+  st.write('detour key list: {}, type: {}'.format(detour_key_list, type(detour_key_list)))
+  st.write('detour key object: {}, type: {}'.format(detour_key, type(detour_key)))
   
 if selected_row_df:
   st.dataframe(selected_row[0])
